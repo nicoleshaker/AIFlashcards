fd1a8c2be37c193d9bddd69cc27343dbbfb49ac4
Nicole Shaker <nicoleshaker@icloud.com>
2024-08-26 12:48:18 -0400

    edits

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 const firebaseConfig = {
-    apiKey: "AIzaSyDhKVVshUFvvS3SE2UnaB6ZQqqqvCbSGgA",
-    authDomain: "ai-flashcards-e09c7.firebaseapp.com",
-    projectId: "ai-flashcards-e09c7",
-    storageBucket: "ai-flashcards-e09c7.appspot.com",
-    messagingSenderId: "717018758239",
-    appId: "1:717018758239:web:6e1a599454009b776aefea",
-    measurementId: "G-15NX5C58MV"
+    apiKey: "key",
+    authDomain: "key",
+    projectId: "key",
+    storageBucket: "key",
+    messagingSenderId: "key",
+    appId: "key",
+    measurementId: "key"
   };
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
